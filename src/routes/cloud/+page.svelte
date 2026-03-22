aa5c7b923a6361f86e8e4a60029507a32f636254
mackerelsoup <lzttzllzttzl@gmail.com>
2026-03-22 20:05:15 +0800

    Push all

---
+++ src/routes/cloud/+page.svelte
@@ -0,0 +1,64 @@
+<script>
+	import './layout.css';
+</script>
+
+<div class="crt">
+		<div class="flex h-screen items-center justify-center align-items">
+      <pre class="ascii-container" >
+  .oooooo.  
+ d8P'  `Y8b
+888      888
+888      888
+888      888 
+`88b    d88'
+ `Y8bood8P'</pre>
+      <pre class="ascii-container" >
+ .o88o. 
+ 888 `" 
+o888oo  
+ 888    
+ 888    
+ 888    
+o888o</pre>
+      <pre class="ascii-container" >
+ .o88o. 
+ 888 `" 
+o888oo  
+ 888    
+ 888    
+ 888    
+o888o</pre>
+      <pre class="ascii-container" >
+oooo  
+`888  
+ 888  
+ 888  
+ 888  
+ 888  
+o888o </pre>
+      <pre class="ascii-container" >
+ o8o  
+ `"'  
+oooo  
+`888  
+ 888  
+ 888  
+o888o </pre>
+      <pre class="ascii-container" >
+        
+        
+ ooo. .oo. 
+`888P"Y88b  
+ 888   888  
+ 888   888
+o888o o888o</pre>
+      <pre class="ascii-container" >
+        
+        
+ .ooooo.  
+d88' `88b 
+888ooo888 
+888    .o 
+`Y8bod8P'</pre>
+		</div>
+</div>
