5ade6f7da9eb1606a1866870635ec300ef155d64
zt <lzttzllzttzl@gmail.com>
2026-03-27 14:20:27 +0800

    V1

--- src/routes/cloud/+page.svelte
+++ src/routes/cloud/+page.svelte
@@ -1,10 +1,42 @@
-<script>
-	import './layout.css';
+<script lang="ts">
+	import './layout.scss';
+	// import { onMount } from 'svelte';
+
+	// onMount(async () => {
+	// 	const { init } = await import('./border.js');
+	// 	init();
+	// });
+
 </script>
 
-<div class="crt">
-		<div class="flex h-screen items-center justify-center align-items">
-      <pre class="ascii-container" >
+<!-- <div>
+<form id="gui" name="inputs">
+    <table>
+        <tr> <td> <font color= #FF8000>distortion</font> </td>
+            <td> <input type="range" id="distortion" min="0" max="100" value="10"/> 
+        </td> </tr>
+        <tr> <td> <font color= #FF8000>stripes</font> </td>
+            <td> <input type="range" id="stripes"  min="0" max="100" value="70"/> 
+        </td> </tr>
+        <tr> <td> <font color= #FF8000>RGB shift</font> </td>
+            <td> <input type="range" id="rgbshift" min="0" max="100" value="10"/> 
+        </td> </tr>
+    </table>
+</form>
+</div> -->
+
+
+<!-- <canvas id="retro-canvas" style="border: none
+ display: block; width: 100vw; height: 100vh;">
+      
+</canvas> -->
+
+<div class="scanlines">
+    <div class="jpg"></div>
+    <div class="overlay">AV-1</div>
+    <div class="ascii-container">
+      <div class="flex h-screen items-center justify-center align-items">
+      <pre class="ascii-letter" >
   .oooooo.  
  d8P'  `Y8b
 888      888
@@ -12,7 +44,7 @@
 888      888 
 `88b    d88'
  `Y8bood8P'</pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
  .o88o. 
  888 `" 
 o888oo  
@@ -20,7 +52,7 @@ o888oo
  888    
  888    
 o888o</pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
  .o88o. 
  888 `" 
 o888oo  
@@ -28,7 +60,7 @@ o888oo
  888    
  888    
 o888o</pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
 oooo  
 `888  
  888  
@@ -36,7 +68,7 @@ oooo
  888  
  888  
 o888o </pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
  o8o  
  `"'  
 oooo  
@@ -44,7 +76,7 @@ oooo
  888  
  888  
 o888o </pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
         
         
  ooo. .oo. 
@@ -52,7 +84,7 @@ o888o </pre>
  888   888  
  888   888
 o888o o888o</pre>
-      <pre class="ascii-container" >
+      <pre class="ascii-letter" >
         
         
  .ooooo.  
@@ -60,5 +92,8 @@ d88' `88b
 888ooo888 
 888    .o 
 `Y8bod8P'</pre>
-		</div>
+	</div>
 </div>
+</div>
+
+
---
+++ src/routes/cloud/border.js
@@ -0,0 +1,231 @@
+// @ts-nocheck
+// Source - https://stackoverflow.com/a/46866546
+// Posted by Rabbid76, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-27, License - CC BY-SA 4.0
+
+var canvas;
+var gl;
+var prog;
+var bufObj = {};
+var textureObj;
+var maskTextureObj;
+var ShProg = {};
+
+var VS_SOURCE = `
+    precision mediump float;
+    attribute vec2 inPos;
+    varying   vec2 vertPos;
+    void main() {
+        vertPos     = inPos;
+        gl_Position = vec4( inPos, 0.0, 1.0 );
+    }
+`;
+
+var FS_SOURCE = `
+    precision mediump float;
+    varying vec2      vertPos;
+    uniform sampler2D u_texture;
+    uniform float     u_distortion = 70;
+    uniform float     u_stripe = 10;
+    uniform float     u_rgbshift = 10;
+    void main() {
+        vec2 ndc_pos = vertPos;
+        vec2 testVec = ndc_pos.xy / max(abs(ndc_pos.x), abs(ndc_pos.y));
+        float len = max(1.0,length( testVec ));
+        ndc_pos *= mix(1.0, mix(1.0,len,max(abs(ndc_pos.x), abs(ndc_pos.y))), u_distortion);
+        vec2 texCoord = vec2(ndc_pos.s, -ndc_pos.t) * 0.5 + 0.5;
+        float stripTile = texCoord.t * mix(10.0, 100.0, u_stripe);
+        float stripFac = 1.0 + 0.25 * u_stripe * (step(0.5, stripTile-float(int(stripTile))) - 0.5);
+        float texR = texture2D( u_texture, texCoord.st-vec2(u_rgbshift) ).r;
+        float texG = texture2D( u_texture, texCoord.st ).g;
+        float texB = texture2D( u_texture, texCoord.st+vec2(u_rgbshift) ).b;
+        float clip = step(0.0, texCoord.s) * step(texCoord.s, 1.0) * step(0.0, texCoord.t) * step(texCoord.t, 1.0);
+        gl_FragColor  = vec4( vec3(texR, texG, texB) * stripFac * clip, 1.0 );
+    }
+`;
+
+function  render(delteMS){
+
+    var vp = [canvas.width, canvas.height];
+    var distortion = document.getElementById( "distortion" ).value / 100.0;
+    var rgbShift = document.getElementById( "rgbshift" ).value / 1000.0;
+    var stripes = document.getElementById( "stripes" ).value / 100.0;
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.enable( gl.DEPTH_TEST );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    var texUnit = 0;
+    gl.activeTexture( gl.TEXTURE0 + texUnit );
+    gl.bindTexture( gl.TEXTURE_2D, textureObj );
+    ShProg.Use( progDraw );
+    ShProg.SetI1( progDraw, "u_texture", texUnit );
+    ShProg.SetF1( progDraw, "u_distortion", distortion );
+    ShProg.SetF1( progDraw, "u_stripe", stripes );
+    ShProg.SetF1( progDraw, "u_rgbshift", rgbShift );
+    VertexBuffer.Draw( bufRect );
+
+    requestAnimationFrame(render);
+}  
+
+function resize() {
+    vp_size = [window.innerWidth, window.innerHeight];
+    //vp_size = [256, 256]
+    canvas.width = vp_size[0];
+    canvas.height = vp_size[1];
+}
+
+function init() {
+
+    canvas = document.getElementById( "retro-canvas");
+    gl = canvas.getContext( "webgl" ) || canvas.getContext( "experimental-webgl" );
+    //gl = canvas.getContext( "webgl2" );
+    if ( !gl )
+    return;
+
+    var texCX = 128;
+    var texCY = 128;
+    var texPlan = [];
+    for (ix = 0; ix < texCX; ++ix) {
+        for (iy = 0; iy < texCY; ++iy) {
+            var val_x = Math.sin( Math.PI * 6.0 * ix / texCX )
+            var val_y = Math.sin( Math.PI * 6.0 * iy / texCY )
+            texPlan.push( 128 + 127 * val_x, 63, 128 + 127 * val_y, 255 );
+        }
+    }
+
+    textureObj = Texture.LoadTexture2D( "https://raw.githubusercontent.com/Rabbid76/graphics-snippets/master/resource/texture/supermario.jpg" );
+    
+    progDraw = ShProg.Create(
+    [ { source : VS_SOURCE, stage : gl.VERTEX_SHADER },
+        { source : FS_SOURCE, stage : gl.FRAGMENT_SHADER }
+    ] );
+    progDraw.inPos = gl.getAttribLocation( progDraw.progObj, "inPos" );
+    if ( progDraw.progObj == 0 )
+        return;
+
+    bufRect = VertexBuffer.Create(
+    [ { data :  [ -1, -1, 1, -1, 1, 1, -1, 1 ], attrSize : 2, attrLoc : progDraw.inPos } ],
+    [ 0, 1, 2, 0, 2, 3 ] );
+
+    window.onresize = resize;
+    resize();
+    requestAnimationFrame(render);
+}
+
+var ShProg = {
+Create: function (shaderList) {
+    var shaderObjs = [];
+    for (var i_sh = 0; i_sh < shaderList.length; ++i_sh) {
+        var shderObj = this.Compile(shaderList[i_sh].source, shaderList[i_sh].stage);
+        if (shderObj) shaderObjs.push(shderObj);
+    }
+    var prog = {}
+    prog.progObj = this.Link(shaderObjs)
+    if (prog.progObj) {
+        prog.attrInx = {};
+        var noOfAttributes = gl.getProgramParameter(prog.progObj, gl.ACTIVE_ATTRIBUTES);
+        for (var i_n = 0; i_n < noOfAttributes; ++i_n) {
+            var name = gl.getActiveAttrib(prog.progObj, i_n).name;
+            prog.attrInx[name] = gl.getAttribLocation(prog.progObj, name);
+        }
+        prog.uniLoc = {};
+        var noOfUniforms = gl.getProgramParameter(prog.progObj, gl.ACTIVE_UNIFORMS);
+        for (var i_n = 0; i_n < noOfUniforms; ++i_n) {
+            var name = gl.getActiveUniform(prog.progObj, i_n).name;
+            prog.uniLoc[name] = gl.getUniformLocation(prog.progObj, name);
+        }
+    }
+    return prog;
+},
+AttrI: function (prog, name) { return prog.attrInx[name]; },
+UniformL: function (prog, name) { return prog.uniLoc[name]; },
+Use: function (prog) { gl.useProgram(prog.progObj); },
+SetI1: function (prog, name, val) { if (prog.uniLoc[name]) gl.uniform1i(prog.uniLoc[name], val); },
+SetF1: function (prog, name, val) { if (prog.uniLoc[name]) gl.uniform1f(prog.uniLoc[name], val); },
+SetF2: function (prog, name, arr) { if (prog.uniLoc[name]) gl.uniform2fv(prog.uniLoc[name], arr); },
+SetF3: function (prog, name, arr) { if (prog.uniLoc[name]) gl.uniform3fv(prog.uniLoc[name], arr); },
+SetF4: function (prog, name, arr) { if (prog.uniLoc[name]) gl.uniform4fv(prog.uniLoc[name], arr); },
+SetM33: function (prog, name, mat) { if (prog.uniLoc[name]) gl.uniformMatrix3fv(prog.uniLoc[name], false, mat); },
+SetM44: function (prog, name, mat) { if (prog.uniLoc[name]) gl.uniformMatrix4fv(prog.uniLoc[name], false, mat); },
+Compile: function (source, shaderStage) {
+    var shaderScript = document.getElementById(source);
+    if (shaderScript)
+        source = shaderScript.text;
+    var shaderObj = gl.createShader(shaderStage);
+    gl.shaderSource(shaderObj, source);
+    gl.compileShader(shaderObj);
+    var status = gl.getShaderParameter(shaderObj, gl.COMPILE_STATUS);
+    if (!status) alert(gl.getShaderInfoLog(shaderObj));
+    return status ? shaderObj : null;
+},
+Link: function (shaderObjs) {
+    var prog = gl.createProgram();
+    for (var i_sh = 0; i_sh < shaderObjs.length; ++i_sh)
+        gl.attachShader(prog, shaderObjs[i_sh]);
+    gl.linkProgram(prog);
+    status = gl.getProgramParameter(prog, gl.LINK_STATUS);
+    if ( !status ) alert(gl.getProgramInfoLog(prog));
+    return status ? prog : null;
+} };
+
+var VertexBuffer = {
+Create: function(attribs, indices, type) {
+    var buffer = { buf: [], attr: [], inx: gl.createBuffer(), inxLen: indices.length, primitive_type: type ? type : gl.TRIANGLES };
+    for (var i=0; i<attribs.length; ++i) {
+        buffer.buf.push(gl.createBuffer());
+        buffer.attr.push({ size : attribs[i].attrSize, loc : attribs[i].attrLoc, no_of: attribs[i].data.length/attribs[i].attrSize });
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer.buf[i]);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array( attribs[i].data ), gl.STATIC_DRAW);
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    if ( buffer.inxLen > 0 ) {
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer.inx);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array( indices ), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    }
+    return buffer;
+},
+Draw: function(bufObj) {
+    for (var i=0; i<bufObj.buf.length; ++i) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, bufObj.buf[i]);
+        gl.vertexAttribPointer(bufObj.attr[i].loc, bufObj.attr[i].size, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray( bufObj.attr[i].loc);
+    }
+    if ( bufObj.inxLen > 0 ) {
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, bufObj.inx);
+        gl.drawElements(bufObj.primitive_type, bufObj.inxLen, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, null );
+    }
+    else
+        gl.drawArrays(bufObj.primitive_type, 0, bufObj.attr[0].no_of );
+    for (var i=0; i<bufObj.buf.length; ++i)
+        gl.disableVertexAttribArray(bufObj.attr[i].loc);
+    gl.bindBuffer( gl.ARRAY_BUFFER, null );
+} };
+
+var Texture = {};
+Texture.HandleLoadedTexture2D = function( image, texture, flipY ) {
+    gl.activeTexture( gl.TEXTURE0 );
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei( gl.UNPACK_FLIP_Y_WEBGL, flipY != undefined && flipY == true );
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE );
+    gl.bindTexture( gl.TEXTURE_2D, null );
+    return texture;
+}
+Texture.LoadTexture2D = function( name ) {
+    var texture = gl.createTexture();
+    texture.image = new Image();
+    texture.image.setAttribute('crossorigin', 'anonymous');
+    texture.image.onload = function () {
+        Texture.HandleLoadedTexture2D( texture.image, texture, false )
+    }
+    texture.image.src = name;
+    return texture;
+}
+
+export { init };
